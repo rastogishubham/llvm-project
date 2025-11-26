669e01cf231ec55b6a767b9266b828202493b53f
Shubham Sandeep Rastogi <Shubham.Rastogi@sony.com>
2025-11-26 14:46:41 -0800

    aaaaa

--- llvm/lib/CodeGen/SelectionDAG/DAGCombiner.cpp
+++ llvm/lib/CodeGen/SelectionDAG/DAGCombiner.cpp
@@ -14466,9 +14466,9 @@ static SDValue tryToFoldExtOfLoad(SelectionDAG &DAG, DAGCombiner &Combiner,
 
   //     // Create a new SDDbgValue that points at the widened node with the
   //     // fragment.
-  //     SDDbgValue *NewDV = DAG.getDbgValue(
-  //         Dbg->getVariable(), NewE, To64.getNode(), To64.getResNo(),
-  //         Dbg->isIndirect(), Dbg->getDebugLoc(), Dbg->getOrder());
+      SDDbgValue *NewDV = DAG.getDbgValue(
+          Dbg->getVariable(), NewE, To64.getNode(), To64.getResNo(),
+          Dbg->isIndirect(), Dbg->getDebugLoc(), Dbg->getOrder());
   //     DAG.AddDbgValue(NewDV, /*isParametet*/ false);
     }
   };
