20b9242dcdd4057c74be63e56d35325aaebb21e0
Shubham Sandeep Rastogi <Shubham.Rastogi@sony.com>
2025-11-26 17:13:37 -0800

    aaa

--- llvm/lib/CodeGen/SelectionDAG/DAGCombiner.cpp
+++ llvm/lib/CodeGen/SelectionDAG/DAGCombiner.cpp
@@ -14456,22 +14456,23 @@ static SDValue tryToFoldExtOfLoad(SelectionDAG &DAG, DAGCombiner &Combiner,
   // the dbg_value attached to the load will be of a smaller bit width, and we
   // have to add a DW_OP_LLVM_fragment to the DIExpression.
   auto SalvageToOldLoadSize = [&](SDValue From, SDValue To64) {
-  //   for (SDDbgValue *Dbg : DAG.GetDbgValues(From.getNode())) {
-  //     unsigned VarBits = From->getValueSizeInBits(0);
+    for (SDDbgValue *Dbg : DAG.GetDbgValues(From.getNode())) {
+      //     unsigned VarBits = From->getValueSizeInBits(0);
 
-  //     // Build/append a fragment expression [0, VarBits]
-  //     const DIExpression *OldE = Dbg->getExpression();
-  //     auto NewE = DIExpression::createFragmentExpression(OldE, 0, VarBits);
+      //     // Build/append a fragment expression [0, VarBits]
+      //     const DIExpression *OldE = Dbg->getExpression();
+      //     auto NewE = DIExpression::createFragmentExpression(OldE, 0,
+      //     VarBits);
 
-  //     // Create a new SDDbgValue that points at the widened node with the
-  //     // fragment.
-  //     if (!NewE)
-  //       continue;
-  //     SDDbgValue *NewDV = DAG.getDbgValue(
-  //         Dbg->getVariable(), *NewE, To64.getNode(), To64.getResNo(),
-  //         Dbg->isIndirect(), Dbg->getDebugLoc(), Dbg->getOrder());
-  //     DAG.AddDbgValue(NewDV, /*isParametet*/ false);
-  //   }
+      //     // Create a new SDDbgValue that points at the widened node with the
+      //     // fragment.
+      //     if (!NewE)
+      //       continue;
+      //     SDDbgValue *NewDV = DAG.getDbgValue(
+      //         Dbg->getVariable(), *NewE, To64.getNode(), To64.getResNo(),
+      //         Dbg->isIndirect(), Dbg->getDebugLoc(), Dbg->getOrder());
+      //     DAG.AddDbgValue(NewDV, /*isParametet*/ false);
+    }
   };
 
   if (NoReplaceTrunc) {
